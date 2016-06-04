7a035ca0a6e52a306398b3267907f8801f8ae1f0
Neithenn <ezequiel.r26@gmail.com>
2016-06-04 20:30:50 -0300

    post stetic and images

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,5 @@ module Blog
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.i18n.default_locale = :es
-    config.assets.paths << Rails.root.join("app", "assets", "fonts")
   end
 end

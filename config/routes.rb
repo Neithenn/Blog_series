fca6c2535bbedaa64be473042c20e193bbd68412
Neithenn <ezequiel.r26@gmail.com>
2016-07-07 21:16:23 -0300

    email notifier new user

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'dashboard/index'
 
-  devise_for :users, controllers: {omniauth_callbacks: "omniauth_callbacks" }
+  devise_for :users, controllers: {omniauth_callbacks: "omniauth_callbacks", :registrations => "my_registrations" }
 
 
   resources :posts do
